d20699219113d033b240f1806bf8c8a1b1cb5b0a
Óscar <oscrudi@users.noreply.github.com>
2020-05-06 21:20:51 +0200

    Update tipo_ruta.php

--- php/gestion_datos/tipo_ruta.php
+++ php/gestion_datos/tipo_ruta.php
@@ -1,20 +1,12 @@
 <?php
 
-    function printTipoRuta($result) {
-        $output = "";
-        while( $row = $result->fetch_assoc() ){
-            $output .= "Código: " . $row["codigo"] . " - Nombre: " . $row["nombre"] . "<br>";
-        }
-        return $output;
-    }
-
     function getAllTipoRuta(){
-        $query = "SELECT codigo, nombre FROM tipo_ruta ORDER BY codigo;";
+        $query = "SELECT * FROM tipo_ruta ORDER BY codigo;";
         return consultarBBDD($query);
     }
 
     function getTipoRutaPorCodigo($codigo){
-        $query = "SELECT codigo, nombre FROM tipo_ruta WHERE codigo = '" . $codigo . "';";
+        $query = "SELECT * FROM tipo_ruta WHERE codigo = '" . $codigo . "';";
         return consultarBBDD($query);
     }
 
