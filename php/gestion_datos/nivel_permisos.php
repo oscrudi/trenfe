0e59d4cb6c8a9cfa31c54dd5f88204693a964243
Óscar <oscrudi@users.noreply.github.com>
2020-05-06 21:17:49 +0200

    Update nivel_permisos.php

--- php/gestion_datos/nivel_permisos.php
+++ php/gestion_datos/nivel_permisos.php
@@ -21,7 +21,6 @@
     }
 
     function desactivarNivelPermisos($codigo){
-        //TODO: Desactivar usuarios con este nivel de permisos
         $query = "UPDATE nivel_permisos SET activo = 0 WHERE codigo = '" . $codigo . "';";
         return modificarBBDD($query);
     }
@@ -34,14 +33,6 @@
     function deletePermisoNivelPermisos($nivel_permisos, $permiso){
         $query = "DELETE FROM permiso_nivel_permisos WHERE nivel_permisos = " . $nivel_permisos . " AND permiso = " . $permiso . ";";
         return modificarBBDD($query);
-    }
-
-    function printNivelPermisos($result){
-        $output = "";
-        while( $row = $result->fetch_assoc() ){
-            $output .= "Código: " . $row["codigo"] . " - Nombre: " . $row["nombre"] . " - Activo: " . $row["activo"] . "<br>";
-        }
-        return $output;
     }
 
     function getAllNivelPermisos($activo = false){
@@ -68,19 +59,13 @@
     }
 
     function getNivelPermisosPorPermiso($permiso){
-        $query = "SELECT np.codigo, np.nombre, np.activo FROM nivel_permisos AS np INNER JOIN permiso_nivel_permisos AS pnp ON np.codigo = pnp.nivel_permisos WHERE pnp.permiso = '" . $permiso . "';";
+        $query = "SELECT np.* FROM nivel_permisos AS np INNER JOIN permiso_nivel_permisos AS pnp ON np.codigo = pnp.nivel_permisos WHERE pnp.permiso = '" . $permiso . "';";
         return consultarBBDD($query);
     }
 
     function getPermisosPorNivelPermisos($nivel_permisos){
-        $query = "SELECT p.codigo, p.nombre FROM permiso AS p INNER JOIN permiso_nivel_permisos AS pnp ON p.codigo = pnp.permiso WHERE pnp.nivel_permisos = '" . $nivel_permisos . "';";
+        $query = "SELECT p.* FROM permiso AS p INNER JOIN permiso_nivel_permisos AS pnp ON p.codigo = pnp.permiso WHERE pnp.nivel_permisos = '" . $nivel_permisos . "';";
         return consultarBBDD($query);
     }
-
-    //TODO: getNivelPermisosPorUsuario($usuario)
-    // function getPermisosPorUsuario($usuario){
-    //     $query = "SELECT p.codigo, p.nombre FROM permiso AS p INNER JOIN permiso_nivel_permisos AS pnp ON p.codigo = pnp.permiso WHERE pnp.nivel_permisos = '" . $nivel_permisos . "';";
-    //     return consultarBBDD($query);
-    // }
 
 ?>
