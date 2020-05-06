6d5e124beac8d5c7981a58ae9308f0f35d15d98a
Óscar <oscrudi@users.noreply.github.com>
2020-05-06 21:23:55 +0200

    Update tipo_vagon.php

--- php/gestion_datos/tipo_vagon.php
+++ php/gestion_datos/tipo_vagon.php
@@ -25,31 +25,23 @@
         return modificarBBDD($query);
     }
 
-    function printTipoVagon($result) {
-        $output = "";
-        while( $row = $result->fetch_assoc() ){
-            $output .= "Código: " . $row["codigo"] . " - Nombre: " . $row["nombre"] . " - Asientos Máx.: " . $row["asientos_max"] . " - Carga Máx.: " . $row["carga_max"] . "<br>";
-        }
-        return $output;
-    }
-
     function getAllTipoVagon(){
-        $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon ORDER BY nombre;";
+        $query = "SELECT * FROM tipo_vagon ORDER BY nombre;";
         return consultarBBDD($query);
     }
 
     function getTipoVagonPorCodigo($codigo){
-        $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon WHERE codigo = '" . $codigo . "';";
+        $query = "SELECT * FROM tipo_vagon WHERE codigo = '" . $codigo . "';";
         return consultarBBDD($query);
     }
 
     function getTipoVagonPorAsientos($asientos_min = null, $asientos_max = null){
         if( $asientos_min && $asientos_min > 0 && $asientos_max && $asientos_max > 0 ){
-            $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon WHERE asientos_max >= " . $asientos_min . " AND asientos_max <= " . $asientos_max . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_vagon WHERE asientos_max >= " . $asientos_min . " AND asientos_max <= " . $asientos_max . " ORDER BY nombre;";
         }elseif( $asientos_min && $asientos_min > 0 ){
-            $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon WHERE asientos_max >= " . $asientos_min . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_vagon WHERE asientos_max >= " . $asientos_min . " ORDER BY nombre;";
         }elseif( $asientos_max && $asientos_max > 0 ){
-            $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon WHERE asientos_max <= " . $asientos_max . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_vagon WHERE asientos_max <= " . $asientos_max . " ORDER BY nombre;";
         }else{
             return getAllTipoVagon();
         }
@@ -58,11 +50,11 @@
 
     function getTipoVagonPorCarga($carga_min = null, $carga_max = null){
         if( $carga_min && $carga_min > 0 && $carga_max && $carga_max > 0 ){
-            $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon WHERE carga_max >= " . $carga_min . " AND carga_max <= " . $carga_max . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_vagon WHERE carga_max >= " . $carga_min . " AND carga_max <= " . $carga_max . " ORDER BY nombre;";
         }elseif( $carga_min && $carga_min > 0 ){
-            $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon WHERE carga_max >= " . $carga_min . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_vagon WHERE carga_max >= " . $carga_min . " ORDER BY nombre;";
         }elseif( $carga_max && $carga_max > 0 ){
-            $query = "SELECT codigo, nombre, asientos_max, carga_max FROM tipo_vagon WHERE carga_max <= " . $carga_max . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_vagon WHERE carga_max <= " . $carga_max . " ORDER BY nombre;";
         }else{
             return getAllTipoVagon();
         }
