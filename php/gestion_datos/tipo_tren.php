a36fab9e01ba04135cb59043dacb49d8bb8681a3
Óscar <oscrudi@users.noreply.github.com>
2020-05-06 21:23:34 +0200

    Update tipo_tren.php

--- php/gestion_datos/tipo_tren.php
+++ php/gestion_datos/tipo_tren.php
@@ -20,31 +20,23 @@
         return modificarBBDD($query);
     }
 
-    function printTipoTren($result) {
-        $output = "";
-        while( $row = $result->fetch_assoc() ){
-            $output .= "Código: " . $row["codigo"] . " - Nombre: " . $row["nombre"] . " - Vagones Máx.: " . $row["vagones_max"] . "<br>";
-        }
-        return $output;
-    }
-
     function getAllTipoTren(){
-        $query = "SELECT codigo, nombre, vagones_max FROM tipo_tren ORDER BY nombre;";
+        $query = "SELECT * FROM tipo_tren ORDER BY nombre;";
         return consultarBBDD($query);
     }
 
     function getTipoTrenPorCodigo($codigo){
-        $query = "SELECT codigo, nombre, vagones_max FROM tipo_tren WHERE codigo = '" . $codigo . "';";
+        $query = "SELECT * FROM tipo_tren WHERE codigo = '" . $codigo . "';";
         return consultarBBDD($query);
     }
 
     function getTipoTrenPorVagones($vagones_min = null, $vagones_max = null){
         if( $vagones_min && $vagones_min > 0 && $vagones_max && $vagones_max > 0 ){
-            $query = "SELECT codigo, nombre, vagones_max FROM tipo_tren WHERE vagones_max >= " . $vagones_min . " AND vagones_max <= " . $vagones_max . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_tren WHERE vagones_max >= " . $vagones_min . " AND vagones_max <= " . $vagones_max . " ORDER BY nombre;";
         }elseif( $vagones_min && $vagones_min > 0 ){
-            $query = "SELECT codigo, nombre, vagones_max FROM tipo_tren WHERE vagones_max >= " . $vagones_min . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_tren WHERE vagones_max >= " . $vagones_min . " ORDER BY nombre;";
         }elseif( $vagones_max && $vagones_max > 0 ){
-            $query = "SELECT codigo, nombre, vagones_max FROM tipo_tren WHERE vagones_max <= " . $vagones_max . " ORDER BY nombre;";
+            $query = "SELECT * FROM tipo_tren WHERE vagones_max <= " . $vagones_max . " ORDER BY nombre;";
         }else{
             return getAllTipoTren();
         }
